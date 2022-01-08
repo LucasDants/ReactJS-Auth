66d2bab9243a9c4d0db4cdf8c6bb55d05638df5d
Lucas Dantas <lucasdantasmoura01@gmail.com>
2022-01-08 14:36:30 -0300

    Controle de permissões

--- contexts/AuthContext.tsx
+++ contexts/AuthContext.tsx
@@ -16,6 +16,7 @@ type SignInCredentials = {
 
 type AuthContextData = {
     signIn(credentials: SignInCredentials): Promise<void>
+    signOut(): void
     isAuthenticated: boolean
     user: User
 }
@@ -26,9 +27,14 @@ type AuthProviderProps = {
 
 export const AuthContext = createContext({} as AuthContextData)
 
+let authChannel: BroadcastChannel
+
 export function signOut() {
     destroyCookie(undefined, 'nextauth.token')
     destroyCookie(undefined, 'nextauth.refreshToken')
+
+    authChannel.postMessage('signOut')
+
     Router.push('/')
 }
 
@@ -36,6 +42,19 @@ export function AuthProvider({ children }: AuthProviderProps) {
     const [user, setUser] = useState<User>()
 
     const isAuthenticated = !!user
+
+    useEffect(() => {
+        authChannel = new BroadcastChannel('auth')
+        authChannel.onmessage = (message) => {
+            switch(message.data) {
+                case 'signOut': 
+                    signOut();
+                    break;
+                default:
+                    break
+            }
+        }
+    }, [])
 
     useEffect(() => {
         const { 'nextauth.token': token } = parseCookies()
@@ -72,7 +91,7 @@ export function AuthProvider({ children }: AuthProviderProps) {
            })
 
            setUser({
-               email,
+                email,
                 permissions,
                 roles
            })
@@ -81,13 +100,15 @@ export function AuthProvider({ children }: AuthProviderProps) {
 
            Router.push('/dashboard')
 
+           authChannel.postMessage('signIn')
+
        } catch (err) {
             console.log(err)
        }
     }
 
     return (
-        <AuthContext.Provider value={{ signIn, isAuthenticated, user }}>
+        <AuthContext.Provider value={{ signIn, signOut, isAuthenticated, user }}>
             { children }
         </AuthContext.Provider>
     )
--- pages/dashboard.tsx
+++ pages/dashboard.tsx
@@ -1,12 +1,18 @@
 import { destroyCookie } from "nookies"
 import { useContext, useEffect } from "react"
+import { Can } from "../components/Can"
 import { AuthContext } from "../contexts/AuthContext"
+import { useCan } from "../hooks/useCan"
 import { setupAPIClient } from "../services/api"
 import { api } from "../services/apiClient"
 import { withSSRAuth } from "../utils/withSSRAuth"
 
 export default function Dashboard() {
-    const { user } = useContext(AuthContext)
+    const { user, signOut } = useContext(AuthContext)
+
+    const userCanSeeMetrics = useCan({
+        permissions: ['metrics.list'],
+    })
 
     useEffect(() => {
         api.get('/me')
@@ -14,7 +20,14 @@ export default function Dashboard() {
     }, [])
 
     return (
-        <h1>{user?.email}</h1>
+       <>
+            <h1>{user?.email}</h1>
+            {userCanSeeMetrics && <div>Métricas</div>}
+            <Can permissions={['metrics.list']}>
+                <div>Métricas</div>
+            </Can>
+            <button onClick={signOut}>Sign Out</button>
+       </>
     )
 }
 
